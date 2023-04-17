a84f63853ccb4b2b20e25d24b8b9a9e90787a38d
ismaelerpistolas <130365680+ismaelerpistolas@users.noreply.github.com>
2023-04-17 13:30:06 +0200

    Update mainficheros.java

--- mainficheros.java
+++ mainficheros.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+	System.out.println("Procesando");
 	System.out.println("Computando");
         Salida.escribir();
         System.out.println("Computando");
